97f9a47115b7b720e5dc3541372b0a1d46e38080
delisartika <delisartika925@gmail.com>
2024-05-28 22:24:37 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\ReviewController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', function () {
+    return view('product');
+});
+
+Route::get('/movies', [MovieController::class, 'index']);
+
+Route::get('/genres', [GenreController::class, 'index']);
+
+Route::get('/reviews', [ReviewController::class, 'index']);
